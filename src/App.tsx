810a5d776cda428def71fa22f84e57a29d946ad2
Gabriel Ribeiro <gabriellribeiro208@gmail.com>
2021-06-22 22:23:36 -0300

    2° aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import {  BrowserRouter, Route } from "react-router-dom"
+import { AuthContextProvider } from "./context/AuthContext";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />v
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
